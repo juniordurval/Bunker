29a26c0f25661fefe5562a14319145f9998032db
juniordurval <160273751+juniordurval@users.noreply.github.com>
2024-02-18 15:13:40 -0330

    Descripción de los cambios realizados

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    ajustarImagen();
+});
+
 window.addEventListener('resize', ajustarImagen);
 
 function ajustarImagen() {
     var imagen = document.querySelector('.imagen-inicial img');
-    if (window.innerWidth / window.innerHeight > imagen.width / imagen.height) {
+    if (imagen && window.innerWidth / window.innerHeight > imagen.width / imagen.height) {
         imagen.style.width = '100%';
         imagen.style.height = 'auto';
-    } else {
+    } else if (imagen) {
         imagen.style.width = 'auto';
         imagen.style.height = '100%';
     }
 }
-
-ajustarImagen();
