d35f2a752cf6c39981076e6c225522c322091150
juniordurval <160273751+juniordurval@users.noreply.github.com>
2024-02-18 16:45:30 -0330

    Se ajusto el diseño css

--- js/script.js
+++ js/script.js
@@ -2,14 +2,16 @@ window.addEventListener('resize', ajustarImagen);
 window.addEventListener('scroll', ajustarImagen);
 
 function ajustarImagen() {
-    var imagen = document.querySelector('.imagen-inicial img');
-    if (imagen && window.innerWidth / window.innerHeight > imagen.width / imagen.height) {
-        imagen.style.width = '100%';
-        imagen.style.height = 'auto';
-    } else if (imagen) {
-        imagen.style.width = 'auto';
-        imagen.style.height = '100%';
-    }
+    var imagenes = document.querySelectorAll('.imagen-inicial, .imagen-secundaria');
+    imagenes.forEach(function(imagen) {
+        if (window.innerWidth / window.innerHeight > imagen.width / imagen.height) {
+            imagen.style.width = '100%';
+            imagen.style.height = 'auto';
+        } else {
+            imagen.style.width = 'auto';
+            imagen.style.height = '100%';
+        }
+    });
 }
 
 ajustarImagen();
