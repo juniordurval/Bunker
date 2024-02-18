8bf5d76bb604f91451263a9795204ce6386204db
juniordurval <160273751+juniordurval@users.noreply.github.com>
2024-02-18 14:18:58 -0330

    Mi primer commit

---
+++ js/script.js
@@ -0,0 +1,2 @@
+// Este es un archivo JavaScript vacío. Puedes agregar tu propio código aquí.
+// Por ejemplo, podrías usar JavaScript para hacer que tu página web sea más interactiva, como responder a clics de botones, etc.
